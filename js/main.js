a8e57ddcaa290be3aec13a584a77d1073e2100a1
najmul57 <najmulcse2@gmail.com>
2023-12-14 13:34:09 +0600

    Landing Page design

--- js/main.js
+++ js/main.js
@@ -11,7 +11,26 @@
     }
     scroll();
 
+    //slider
+    $('.slide__active').slick({
+        dots: false,
+        arrows: false,
+        infinite: false,
+        slidesToShow: 1,
+        autoplay: true,
+        fade: true,
+        slidesToScroll: 1,
+        speed: 500,
+        cssEase: 'linear'
+    });
 
+    //counter up
+
+
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
 
 
 
